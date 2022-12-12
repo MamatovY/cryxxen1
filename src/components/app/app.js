caf29de4e03216a843aeb3d7525067c8916e4484
MamatovY <mamatovyiman28@gmail.com>
2022-12-12 09:37:38 +0600

    finish all

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,10 +14,7 @@ import Footer from '../footer';
 
 
 
-//Сообщение к разработчику
-console.log(
-  '%c Старался сделать сайт гибким. Попробовал использовать везде проценты: margin,padding,width,height,gap ',
-  'background: #222; color: #bada55');
+
 
 
 function App() {
